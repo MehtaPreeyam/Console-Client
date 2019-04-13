761f07a559057b8f98b619999fa3b777e2a3a757
BuildTools <unconfigured@null.spigotmc.org>
2019-04-13 09:30:38 -0500

    added config

--- MinecraftClient/ChatBots/MoneyBot.cs
+++ MinecraftClient/ChatBots/MoneyBot.cs
@@ -32,15 +32,13 @@ namespace MinecraftClient.ChatBots
                 String[] elements = text.Split(' ');
                 String amt = elements[0];
                 String name = text.Substring(text.IndexOf(">") + 2, text.Length - text.IndexOf(">") - 2);
-                // int amount = Int32.Parse(amt.Substring(1, amt.Length));
                 save(amt.Substring(1) + " " + name);
             }
         }
 
         private void save(string tosave)
         {
-             tosave = GetTimestamp() + ' ' + tosave;
-
+            tosave = GetTimestamp() + ' ' + tosave;
             string directory = Path.GetDirectoryName(logfile);
             if (!String.IsNullOrEmpty(directory) && !Directory.Exists(directory))
                 Directory.CreateDirectory(directory);
